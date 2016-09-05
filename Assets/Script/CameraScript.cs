34e17939b31d54a11f4f0a467008a3610279ad72
Avikash Mishra <avimish62@gmail.com>
2016-09-05 20:24:31 +1200

    Path generation working

--- Assets/Script/CameraScript.cs
+++ Assets/Script/CameraScript.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class CameraScript : MonoBehaviour {
-    public float movementSpeed = 7f;
-    private float tiltSpeed = 10f;
+    public float movementSpeed = 10f;
+    private float tiltSpeed = 20f;
 
     // Use this for initialization
     void Start () {
@@ -14,6 +14,6 @@ public class CameraScript : MonoBehaviour {
 	void Update () {
         float tiltX = Input.acceleration.x;
         transform.Translate(0,0,Time.deltaTime * movementSpeed);
-        transform.Translate(tiltX * tiltSpeed* Time.deltaTime, 0,0);
+        transform.Translate(tiltX * tiltSpeed * Time.deltaTime, 0,0.1f);
     }
 }
--- Assets/Script/PathGenerator.cs
+++ Assets/Script/PathGenerator.cs
@@ -4,33 +4,99 @@ using System.Collections;
 public class PathGenerator : MonoBehaviour {
 
     public GameObject block;
+    private float timeTillSecondPath;
     private ArrayList blockPath = new ArrayList();
     private int pathWidth = 7;
-    private int cubeZDistance = 2;
-    private int pathX = 0; 
+    private int offSet = 2;
+    private int cubeDistance = 2;
+    private float zPosEnd = 15;
+
+    private int currentPath = 1; 
 
     // Use this for initialization
     void Start () {
-        blockPath = new ArrayList();
-        for(int i = 0; i < Constants.WORLD.blockLengthPathStart; i++) {
-            var posX = pathX;
-            var posZ = (i + 1) * cubeZDistance;
-            var posY = 0;
+        timeTillSecondPath = 10.0f;
+        generateStraightPath();
 
-            GameObject pathBlockRight = Instantiate(block, new Vector3(posX + pathWidth, posY, posZ), Quaternion.identity) as GameObject;
-            GameObject pathBlockLeft = Instantiate(block, new Vector3(-(posX + pathWidth), posY, posZ), Quaternion.identity) as GameObject;
-            if (pathBlockRight != null) {
-                blockPath.Add(pathBlockRight);
-            }
-            if (pathBlockLeft != null) {
-                blockPath.Add(pathBlockLeft);
-            }
-
-        }   
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        	
-	}
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        if (playerPosition.z > (zPosEnd - 15)) {
+            switch (currentPath) {
+                case 1:
+                    generateZigZagPath();
+                    currentPath = 2;
+                    break;
+                case 2:
+                    generateStraightPath();
+                    currentPath = 1;
+                    break;
+            }
+        }
+
+    }
+
+    public void generateStraightPath() {
+        pathWidth = 7;
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        float playerX = playerPosition.x;
+        float playerZ = zPosEnd;
+        float playerY = 0;
+
+        float posZ = playerZ;
+        float posX = playerX;
+        float posY = 0;
+        for (int i = 0; i < Constants.WORLD.blockLengthPathStart; i++) {
+            posZ += cubeDistance;
+
+            GameObject pathBlockRight = Instantiate(block, new Vector3(posX + pathWidth - offSet, posY, posZ), Quaternion.identity) as GameObject;
+            GameObject pathBlockLeft = Instantiate(block, new Vector3(posX - pathWidth - offSet, posY, posZ), Quaternion.identity) as GameObject;
+
+        }
+        zPosEnd = posZ;
+    }
+
+    public void generateZigZagPath() {
+        pathWidth = 10;
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        float playerX = playerPosition.x;
+        float playerZ = zPosEnd;
+        float playerY = 0;
+
+        float posZ = playerZ;
+        float posX = playerX;
+        float posY = 0;
+
+        //going stright
+        for (int i = 0; i < 15; i++) {
+            posZ += cubeDistance;
+            GameObject pathBlockRight = Instantiate(block, new Vector3(posX + pathWidth - offSet, posY, posZ), Quaternion.identity) as GameObject;
+            GameObject pathBlockLeft = Instantiate(block, new Vector3(posX - pathWidth - offSet, posY, posZ), Quaternion.identity) as GameObject;
+                    
+        }
+
+        //turning right
+        for (int i = 0; i < 25; i++) {
+            posX ++;
+            posZ += cubeDistance;         
+            GameObject pathBlockRight = Instantiate(block, new Vector3(posX + pathWidth - offSet, posY, posZ), Quaternion.identity) as GameObject;
+            GameObject pathBlockLeft = Instantiate(block, new Vector3(posX - pathWidth - offSet, posY, posZ), Quaternion.identity) as GameObject;
+        }
+
+        //turning left
+        for (int i = 0; i < 25; i++) {
+            posX--;
+            posZ += cubeDistance;
+            GameObject pathBlockRight = Instantiate(block, new Vector3(posX + pathWidth - offSet, posY, posZ), Quaternion.identity) as GameObject;
+            GameObject pathBlockLeft = Instantiate(block, new Vector3(posX - pathWidth - offSet, posY, posZ), Quaternion.identity) as GameObject;
+        }
+        zPosEnd = posZ;
+
+    }
 }
